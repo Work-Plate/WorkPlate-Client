f1fa8dd7080d062dda19b2bbd85f6b4c811c0ee6
sayyyho <323psh@naver.com>
2024-11-13 16:06:51 +0900

    [Feature] #14 - color, fonts theme 초기 설정 및 메인 텍스트 구현

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -4,19 +4,16 @@ import { Header } from "@components/header/Header";
 import { BottomNavigation } from "@components/common/bottomNavigation/BottomNavigation";
 export const Main = () => {
   return (
-    <>
-      <S.Layout>
-        <Header />
-        <div
-          style={{
-            padding: "1rem",
-            margin: "1rem 0",
-          }}
-        >
-          메인이지롱
-        </div>
-        <BottomNavigation />
-      </S.Layout>
-    </>
+    <S.Layout>
+      <Header />
+      <S.MainContainer>
+        <S.TextWrapper>
+          <S.BodyText>내 엽전</S.BodyText>
+          <S.Title1>15</S.Title1>
+          <S.Title2>개</S.Title2>
+        </S.TextWrapper>
+      </S.MainContainer>
+      <BottomNavigation />
+    </S.Layout>
   );
 };
--- src/pages/main/Main.styled.js
+++ src/pages/main/Main.styled.js
@@ -8,3 +8,36 @@ export const Layout = styled.div`
   align-items: center;
   margin: 5rem 0;
 `;
+
+export const MainContainer = styled.div`
+  width: 90%;
+  padding: 1rem;
+  border-radius: 0.625rem;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+  background-color: #f5f6f9;
+`;
+
+export const TextWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const BodyText = styled.p`
+  ${({ theme }) => theme.fonts.body1};
+  color: ${({ theme }) => theme.colors.deepOrange};
+  margin-right: 0.5rem;
+`;
+
+export const Title1 = styled.p`
+  ${({ theme }) => theme.fonts.title1};
+`;
+
+export const Title2 = styled.p`
+  ${({ theme }) => theme.fonts.title2};
+`;
