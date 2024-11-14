bfadce78f8fb6edea544ce03f7edc63840e0d83f
JunEunChae <celina03240@ewhain.net>
2024-11-14 15:18:45 +0900

    feat : 식당페이지 껍데기 완성

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Splash } from "@pages/splash/Splash";
 import { Main } from "@pages/main/Main";
+import FoodMainPage from "@pages/foodMainPage/FoodMainPage";
+import ChatBotPage from "@pages/chatBotPage/ChatBotPage";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
       {
         path: "/main",
         element: <Main />,
+      },
+      {
+        path: "/food",
+        element: <FoodMainPage />,
+      },
+      {
+        path: "/chatbot",
+        element: <ChatBotPage />,
       },
     ],
   },
