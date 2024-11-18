d5fbf127a3a021e567b4cb08516622487e9b7c00
sayyyho <323psh@naver.com>
2024-11-18 15:47:04 +0900

    [Feature] #14 - 밥상일터 메인 컨텐츠 UI 구현

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -12,6 +12,37 @@ export const Main = () => {
           <S.Title1>15</S.Title1>
           <S.Title2>개</S.Title2>
         </S.TextWrapper>
+        <S.MainBoxWrapper>
+          <S.MainBox $background={"#FF9B5C"}>
+            <S.MainBoxFirstWrapper>
+              <S.MainBoxText1>일거리 찾기</S.MainBoxText1>
+              <S.MainBoxText2>소일거리</S.MainBoxText2>
+            </S.MainBoxFirstWrapper>
+            <S.MainBoxSecondWrapper>
+              <img
+                src="/images/luggage-03.svg"
+                alt="서류 가방"
+                width={"32px"}
+                height={"32px"}
+              />
+            </S.MainBoxSecondWrapper>
+          </S.MainBox>
+          <S.MainBox $background={"#FF854C"}>
+            <S.MainBoxFirstWrapper>
+              <S.MainBoxText1>식당 찾기</S.MainBoxText1>
+              <S.MainBoxText2>밥상</S.MainBoxText2>
+            </S.MainBoxFirstWrapper>
+            <S.MainBoxSecondWrapper>
+              <img
+                src="/images/cutlery-02.svg"
+                alt="숟가락과 포크"
+                width={"32px"}
+                height={"32px"}
+              />
+            </S.MainBoxSecondWrapper>
+          </S.MainBox>
+        </S.MainBoxWrapper>
+        <S.qrButton>QR 코드로 결제하기</S.qrButton>
       </S.MainContainer>
       <BottomNavigation />
     </S.Layout>
--- src/pages/main/Main.styled.js
+++ src/pages/main/Main.styled.js
@@ -10,6 +10,8 @@ export const Layout = styled.div`
 `;
 
 export const MainContainer = styled.div`
+  margin-top: 1.5rem;
+
   width: 90%;
   padding: 1rem;
   border-radius: 0.625rem;
@@ -40,4 +42,74 @@ export const Title1 = styled.p`
 
 export const Title2 = styled.p`
   ${({ theme }) => theme.fonts.title2};
+`;
+
+export const MainBoxWrapper = styled.div`
+  display: flex;
+  height: 120px;
+  gap: 1rem;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MainBox = styled.div`
+  background: ${({ $background }) => $background};
+  border-radius: 10px;
+  height: 100%;
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const MainBoxFirstWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  flex-direction: column;
+  padding: 1rem 0 0 1rem;
+  justify-content: center;
+`;
+
+export const MainBoxSecondWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+  padding: 0 1rem 1rem 0;
+`;
+
+export const qrButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  width: calc(90% + 1rem);
+  padding: 1rem 2.5rem;
+  background-color: #415058;
+  color: white;
+
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const MainBoxText1 = styled.p`
+  color: white;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.28px;
+`;
+
+export const MainBoxText2 = styled.p`
+  color: white;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -0.48px;
 `;
